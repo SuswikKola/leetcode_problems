991cda58322583fa88327761ef07b1a37e451808
SuswikKola <150536083+SuswikKola@users.noreply.github.com>
2025-05-03 21:54:02 +0530

    Time: 55 ms (90.00%) | Memory: 29.5 MB (67.83%) - LeetSync

--- 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.py
+++ 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.py
@@ -2,11 +2,7 @@ class Solution:
     def findValueOfPartition(self, nums: List[int]) -> int:
         nums.sort()
         res = float('inf')
-        i=0
-        j=i+1
-        while j<len(nums):
-            if (nums[j]-nums[i])<res:
-                res = nums[j]-nums[i]
-            i+=1
-            j+=1
+        for i in range(len(nums)-1):
+            if (nums[i+1]-nums[i])<res:
+                res = nums[i+1]-nums[i]
         return res
