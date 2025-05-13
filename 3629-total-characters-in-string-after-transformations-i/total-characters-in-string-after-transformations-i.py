299417b2d62cbdcf30d7b50deb8061d677e126fe
SuswikKola <150536083+SuswikKola@users.noreply.github.com>
2025-05-13 15:00:55 +0530

    Time: 5187 ms (32.03%) | Memory: 18.2 MB (74.51%) - LeetSync

---
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        MOD = 10**9 + 7
+        freq = [0] * 26
+        for c in s:
+            freq[ord(c) - ord('a')] += 1
+
+        for _ in range(t):
+            new_freq = [0] * 26
+            for i in range(26):
+                if i == 25:
+                    new_freq[0] = (new_freq[0] + freq[25]) % MOD
+                    new_freq[1] = (new_freq[1] + freq[25]) % MOD
+                else:
+                    new_freq[i+1] = (new_freq[i+1] + freq[i]) % MOD
+            freq = new_freq
+
+        return sum(freq) % MOD
