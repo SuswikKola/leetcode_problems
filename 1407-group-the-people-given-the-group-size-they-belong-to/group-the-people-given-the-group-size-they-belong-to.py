ae4e4ad9dad0ee1b740ac4f7932addd4a821361c
Suswik Kola <150536083+SuswikKola@users.noreply.github.com>
2025-05-26 22:26:12 +0530

    Time: 4 ms (28.56%) | Memory: 17.9 MB (72.69%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -1,20 +1,23 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        res = []
         dict_a = {}
         for i,e in enumerate(groupSizes):
             if e not in dict_a:
                 dict_a[e]=[]
             dict_a[e].append(i)
-        res = []
-        print(dict_a)
-        for i in dict_a:
-            k = len(dict_a[i])//i
-            t =0
-            while k:
-                temp = dict_a[i][t:t+i]
-                res.append(temp)
-                t+=i
-                k-=1
+            if len(dict_a[e])==e:
+                res.append(dict_a[e])
+                dict_a[e]=[]
+        # res = []
+        # for i in dict_a:
+        #     k = len(dict_a[i])//i
+        #     t =0
+        #     while k:
+        #         temp = dict_a[i][t:t+i]
+        #         res.append(temp)
+        #         t+=i
+        #         k-=1
         return res
 
                 
