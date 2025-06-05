ab417418be6bf17ede533e3bf9ba0b165bc1db82
Suswik Kola <150536083+SuswikKola@users.noreply.github.com>
2025-06-05 22:28:55 +0530

    Time: 6 ms (54.99%) | Memory: 18 MB (21.17%) - LeetSync

---
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
@@ -0,0 +1,41 @@
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+       
+        def find(x):
+            while parent[x] != x:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+            
+        parent = list(range(26))
+        for a, b in zip(s1, s2):
+            x, y = ord(a) - 97, ord(b) - 97
+            rx, ry = find(x), find(y)
+            if rx != ry:
+                if rx < ry:
+                    parent[ry] = rx
+                else:
+                    parent[rx] = ry
+        
+        return ''.join(chr(find(ord(c) - 97) + 97) for c in baseStr)
+
+        # Solution 1:
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        parent = list(range(26))
+        for a, b in zip(s1, s2):
+            x, y = ord(a) - 97, ord(b) - 97
+            rx, ry = find(x), find(y)
+            if rx != ry:
+                if rx < ry: parent[ry] = rx
+                else:       parent[rx] = ry
+
+        res = []
+        for c in baseStr:
+            r = find(ord(c) - 97)
+            res.append(chr(r + 97))
+
+        return ''.join(res)
