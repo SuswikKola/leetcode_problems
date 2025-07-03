0fb247a38e9ac465ba81c29fe4c0651a6505cf1d
Suswik Kola <150536083+SuswikKola@users.noreply.github.com>
2025-07-03 18:37:16 +0530

    Time: 731 ms (27.94%) | Memory: 24.4 MB (67.40%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,4 +1,33 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        nums.sort()
+        def mergeHalves(nums,lo,mid,hi):
+            temp = []
+            left = lo
+            right = mid+1
+            while(left<=mid and right<=hi):
+                if nums[left]<=nums[right]:
+                    temp.append(nums[left])
+                    left+=1
+                else:
+                    temp.append(nums[right])
+                    right+=1
+            while left<=mid:
+                temp.append(nums[left])
+                left+=1
+            while right<=hi:
+                temp.append(nums[right])
+                right+=1
+            for i in range(lo,hi+1):
+                nums[i] = temp[i-lo]
+        
+        
+        def mergeSort(nums,lo,hi):
+            if lo==hi:
+                return
+            mid = (lo+hi)//2
+            mergeSort(nums,lo,mid)
+            mergeSort(nums,mid+1,hi)
+            mergeHalves(nums,lo,mid,hi)
+        mergeSort(nums,0,len(nums)-1)
         return nums
+        
